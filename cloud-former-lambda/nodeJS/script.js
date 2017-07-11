f244fd27993d376af8ec7a51b5a49d275a178a0b
Rushil Soni <retrofy@users.noreply.github.com>
2017-07-11 15:25:03 +0100

    New stable nodejs code
    Added 2 new intents  for listing templates within an S3 bucket and getting the count of templates in an s3 bucket

--- cloud-former-lambda/nodeJS/script.js
+++ cloud-former-lambda/nodeJS/script.js
@@ -1,9 +1,9 @@
 /*
-* Lambda function code for the CloudBuilder Amazon Alexa Skill
-* Currently Supported Features: Create, Delete, Output to Console (CloudWatch Log)
+* Lambda function code for the CloudFormer Amazon Alexa Skill
+* Currently Supported Features: Create, Delete, List Templates, Template Count and Output to Console (CloudWatch Log)
 *
 * @author rush.soni@capgemini.com
-* @version 1.0
+* @version 1.1
 */
 
 'use strict';
@@ -17,9 +17,10 @@ var Stack = new require('./node_modules/cloudformer-node');
 //Add dependency to Alexa SDK
 var Alexa = new require('./node_modules/alexa-sdk');
 
-//Handling of intents from Alexa Skills
+//Handling of intents from Alexa Skills Kit
 var handlers = {
 
+   //Logic for creating a Stack
   'CloudFormerCreateIntent': function () {
 
     //Initialise Stack instance.
@@ -42,6 +43,7 @@ var handlers = {
 
   },
 
+   //Logic for deleteing Stack
   'CloudFormerDeleteIntent' : function () {
 
     //Initialise Stack instance.
@@ -52,12 +54,134 @@ var handlers = {
     theStack.delete(console.log);
 
     this.emit(':tell', "your stack has been deleted");
+  },
+
+   //Logic for listing the available templates
+  'CloudFormerListTemplateIntent' : function () {
+
+    //Create an object used to access S3
+    var s3 = new AWS.S3({apiVersion: '2006-03-01'});
+
+    //Pass parameters for getting information from buckets
+    var params = {
+      Bucket : "cloudformer-eu-west-1",
+      MaxKeys : 1000
+    };
+
+    //POST a request to aws to list objects in the specified bucket
+    var request = s3.listObjectsV2(params, function (err, data){
+
+      //Get a list of  JSON objects from the bucket
+      if (err) {
+        //an error occured.
+        console.log(err, err.stack); // an error occurred
+      }
+      else{
+        return data;
+      }
+
+    });
+
+    var self = this;
+
+    //success response
+    var success_response = request.on('success', function(response){
+
+      //Succesfully located the bucket an it's contents
+      var template_counter = 1;
+
+
+      var shortPause = "<break time='1s'/>";
+      var speechOutput = 'Here is a list of cloud formation templates available in your S3 bucket' + shortPause;
+
+
+      //for each object within the bucket output it's description
+      for(var i = 0; i < response.data.Contents.length; i++){
+
+          var bucket_item = response.data.Contents[i];
+
+          if(bucket_item.Key.includes(".json")){
+
+             var cloudformation_template = bucket_item;
+
+             //Trims key removing the file extension
+             var cloudformation_template_name = cloudformation_template.Key.replace(/\.[^/.]+$/, "");
+
+             speechOutput +=  template_counter + "; " + cloudformation_template_name + shortPause;
+
+             template_counter += 1;
+
+          }
+
+      }
+
+      self.emit(':tell', speechOutput);
+
+      return;
+
+    });
+
+  },
+
+   //Logic to output the count of JSON templates within an S3 Bucket
+  'CloudFormerOutputTemplateCountIntent' : function () {
+
+    //Create an object used to access S3
+    var s3 = new AWS.S3({apiVersion: '2006-03-01'});
+
+    //Pass parameters for getting information from buckets
+    var params = {
+      Bucket : "cloudformer-eu-west-1",
+      MaxKeys : 1000
+    };
+
+    //POST a request to aws to list objects in the specified bucket
+    var request = s3.listObjectsV2(params, function (err, data){
+
+      //Get a list of  JSON objects from the bucket
+      if (err) {
+        //an error occured.
+        console.log(err, err.stack); // an error occurred
+      }
+      else{
+        return data;
+      }
+
+    });
+
+    var self = this;
+
+    //success response
+    var success_response = request.on('success', function(response){
+
+      //Succesfully located the bucket an it's contents
+      var template_counter = 0;
+
+      //for each object within the bucket output it's description
+      for(var i = 0; i < response.data.Contents.length; i++){
+
+          var bucket_item = response.data.Contents[i];
+
+          if(bucket_item.Key.includes(".json")){
+             template_counter += 1;
+          }
+
+      }
+
+      self.emit(':tell', 'Your S3 bucket has ' + template_counter + " cloud formation templates.");
+
+      return;
+
+    });
+
   }
+
+
 
 }
 
 /*
-* Handler: contains lambda function logic.
+* Register alexa handlers, which deal with intent specific calls for the cloud former skill.
 */
 exports.createCloudHandler = (event, context, callback) => {
 
