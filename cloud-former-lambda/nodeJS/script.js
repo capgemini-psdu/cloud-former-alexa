006a9f70c26bb29f4300ee548c190a20dde80b9d
Rushil Soni <retrofy@users.noreply.github.com>
2017-07-14 17:01:09 +0100

    Version 1.3: Updated code to handle Dynamic Creation/Deletion of Stacks.

--- cloud-former-lambda/nodeJS/script.js
+++ cloud-former-lambda/nodeJS/script.js
@@ -1,10 +1,10 @@
 /*
-* Lambda function code for the CloudFormer Amazon Alexa Skill
-* Currently Supported Features: Create, Delete, List Templates, Template Count and Output to Console (CloudWatch Log)
-*
-* @author rush.soni@capgemini.com
-* @version 1.2
-*/
+ * Lambda function code for the CloudFormer Amazon Alexa Skill
+ * Currently Supported Features: Dynamic Create, Dynamic Delete, List Templates, Template Count and Output to Console (CloudWatch Log)
+ *
+ * @author rush.soni@capgemini.com
+ * @version 1.3
+ */
 
 'use strict';
 
@@ -18,33 +18,35 @@ var Stack = new require('./node_modules/cloudformer-node');
 var Alexa = new require('./node_modules/alexa-sdk');
 
 //Create an object used to access S3
-var s3 = new AWS.S3({apiVersion: '2006-03-01'});
+var s3 = new AWS.S3({
+  apiVersion: '2006-03-01'
+});
 
 var bucket = "cloudformer-eu-west-1";
 
-//Promise used when checking maps accquired using the AWS SDK
+var bucket_region = 'eu-west-1';
+
+//Promise used when checking maps accquired from AWS using the AWS SDK
 var mapPromise = new Promise(
-  function(resolve, reject){
+  function(resolve, reject) {
 
-    var map = produceKeyUrlMap(bucket, 1000);
+    var map = getS3BucketObjects(bucket, 1000);
 
-    if(map != null && map != {}){
+    if (map != null && map != {}) {
       resolve(map);
-    }
-    else {
+    } else {
 
       var errorMsg;
 
-      if(map == {}){
+      if (map == {}) {
         errorMsg = "There are no keys in" + bucket;
-      }
-      else if(map == null){
+      } else if (map == null) {
         errorMsg = "The S3 Bucket you specified doesn't exist."
       }
 
       reject(errorMsg);
     }
-});
+  });
 
 
 //Handling of intents from Alexa Skills Kit
@@ -53,37 +55,96 @@ var handlers = {
   //Logic for creating a Stack
   'CloudFormerCreateIntent': function() {
 
-    //Initialise Stack instance.
-    var stackName = 'cloudformer-stack-test';
-    var theStack = new Stack(stackName);
+    var self = this;
 
-    //Create stack from amazon web services
-    theStack.apply('https://s3-eu-west-1.amazonaws.com/cloudformer-eu-west-1/cloudformer_basic_template.json', {
-      Parameters: {},
-      DisableRollback: false,
-      Capabilities: [],
-      NotificationARNs: [],
-      Tags: {
-        Name: stackName
+    //Function logic for processing mapPromise for Listing maps.
+    mapPromise.then(
+
+      // Success: Case where the mapPromise has been sucessfully resolved and map contains objects..
+      function(map) {
+
+        //Get the user specified option
+        var option_number = self.event.request.intent.slots.OptionNumber.value;
+
+        if(map[option_number] != null)
+        {
+          var stackName = map[option_number].name;
+          var theStack = new Stack(stackName);
+
+          //Create stack from amazon web services
+          theStack.apply(map[option_number].url, {
+            Parameters: {},
+            DisableRollback: false,
+            Capabilities: [],
+            NotificationARNs: [],
+            Tags: {
+              Name: stackName
+            },
+          }, console.log);
+
+          self.emit(':tell', "your stack has been created");
+          return;
+
+         }
+         else
+         {
+           console.error("Item not found within the S3 bucket.");
+           self.emit(':tell', "The option number you specified doesn't exist within the S3 bucket");
+         }
       },
-    }, console.log);
+      // Fail: Case where the map returns empty
+      function(errorMsg) {
 
-    this.emit(':tell', "your stack has been created");
+        //log error in console and then have alexa emit the message.
+        console.error(errorMsg);
+        self.emit(':tell', errorMsg);
+
+      }
+    );
     return;
-
   },
 
   //Logic for deleteing Stack
   'CloudFormerDeleteIntent': function() {
+    var self = this;
 
-    //Initialise Stack instance.
-    var stackName = 'cloudformer-stack-test';
-    var theStack = new Stack(stackName);
+    //Function logic for processing mapPromise for Listing maps.
+    mapPromise.then(
 
-    //Request the stack be deleted.
-    theStack.delete(console.log);
+      // Success: Case where the mapPromise has been sucessfully resolved and map contains objects..
+      function(map) {
 
-    this.emit(':tell', "your stack has been deleted");
+        //Get the user specified option
+        var option_number = self.event.request.intent.slots.OptionNumber.value;
+
+        if(map[option_number] != null)
+        {
+          var stackName = map[option_number].name;
+          var theStack = new Stack(stackName);
+
+          //Create stack from amazon web services
+          theStack.delete(console.log);
+
+          self.emit(':tell', "your stack has been deleted");
+          return;
+
+         }
+         else
+         {
+           console.error("Cannot find stack with the name " + map[option_number].name + "in the list of running stacks.");
+           self.emit(':tell', "Cannot find stack with the name " + map[option_number].name + "in the list of running stacks.");
+         }
+      },
+      // Fail: Case where the map returns empty
+      function(errorMsg) {
+
+        //log error in console and then have alexa emit the message.
+        console.error(errorMsg);
+        self.emit(':tell', errorMsg);
+
+      }
+    );
+    return;
   },
 
   //Logic for listing the available templates
@@ -111,7 +172,7 @@ var handlers = {
         self.emit(':tell', speechOutput);
       },
       // Fail: Case where the map returns empty
-      function(errorMsg){
+      function(errorMsg) {
 
         //log error in console and then have alexa emit the message.
         console.error(new Error(errorMsg));
@@ -120,7 +181,7 @@ var handlers = {
       }
     );
 
-    },
+  },
 
   //Logic to output the count of JSON templates within an S3 Bucket
   'CloudFormerOutputTemplateCountIntent': function() {
@@ -143,10 +204,10 @@ var handlers = {
         self.emit(':tell', 'Your S3 bucket has ' + template_counter + " cloud formation templates.");
       },
       // Case where the map returns empty
-      function(errorMsg){
+      function(errorMsg) {
 
         //log error in console and then have alexa emit the message.
-        console.error(new Error(errorMsg));
+        console.error(errorMsg);
         self.emit(':tell', errorMsg);
 
       }
@@ -155,25 +216,25 @@ var handlers = {
 }
 
 /*
-* Register alexa handlers, which deal with intent specific calls for the cloud former skill.
-*/
+ * Register alexa handlers, which deal with intent specific calls for the cloud former skill.
+ */
 exports.createCloudHandler = (event, context, callback) => {
 
-      var alexa_handler = Alexa.handler(event, context, callback);
-      alexa_handler.appId = 'amzn1.ask.skill.c2500316-170e-41cc-9c25-45788bd2b814';
-      alexa_handler.registerHandlers(handlers);
-      alexa_handler.execute();
+  var alexa_handler = Alexa.handler(event, context, callback);
+  alexa_handler.appId = 'amzn1.ask.skill.c2500316-170e-41cc-9c25-45788bd2b814';
+  alexa_handler.registerHandlers(handlers);
+  alexa_handler.execute();
 
 };
 
 /*
-* Produces a mapping of  (Integer -> CloudFormation Template URL)
-*/
-function produceKeyUrlMap(bucketName, maxKeyCount){
+ * Produces a mapping of  (Integer -> CloudFormation Template URL)
+ */
+function getS3BucketObjects(bucketName, maxKeyCount) {
 
   var params = {
     Bucket: bucketName,
-    MaxKeys : maxKeyCount
+    MaxKeys: maxKeyCount
   };
 
   //Counter used to keep track of read in templates.
@@ -190,38 +251,34 @@ function produceKeyUrlMap(bucketName, maxKeyCount){
 
   return promise.then(
     //Success: successfully returned response object.
-    function(data){
+    function(data) {
       //for each cloudformation template key in the bucket add it to a map.
-      for(var i = 0; i < data.Contents.length; i++){
+      for (var i = 0; i < data.Contents.length; i++) {
 
-          var bucket_item = data.Contents[i];
+        var bucket_item = data.Contents[i];
 
-          if(bucket_item.Key.includes(".json")){
+        if (bucket_item.Key.includes(".json")) {
 
-            //increment counter for new map address.
-            template_counter++;
+          //increment counter for new map address.
+          template_counter++;
 
-             // get the cloud formation key.
-             var cloudformation_template_key = bucket_item.Key;
+          // get the cloud formation key.
+          var cloudformation_template_key = bucket_item.Key;
+          var url = "https://s3-" + bucket_region + ".amazonaws.com/" + bucketName + "/" + cloudformation_template_key;
 
-             //Parameters for GET request for URL associated with the key.
-             var params = {
-               Bucket: 'cloudformer-eu-west-1',
-               Key: cloudformation_template_key
-             };
 
-             //Create a javascript object map to contain
-             bucket_list[template_counter] = {
-               'name': params.Key.replace(/\.[^/.]+$/, ""), //removes extension from file name
-               'url' : s3.getSignedUrl('getObject', params) // gets the signed url.
-             };
-          }
+          //Create a javascript object map to contain
+          bucket_list[template_counter] = {
+            'name': cloudformation_template_key.replace(/\.[^/.]+$/, ""), //removes extension from file name
+            'url': url // link.
+          };
+        }
       }
       return bucket_list;
 
     },
     //Failure: couldn't get a list of objects with the provided parameters.
-    function(error){
+    function(error) {
       console.log("The bucket you provided doesn't exist");
       return null;
     }
